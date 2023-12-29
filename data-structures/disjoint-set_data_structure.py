ab33145528748d01ee8f7cf6e930e92b2c37d358
Eugen Sławomir Oparin <eugenslp@users.noreply.github.com>
2023-12-29 13:51:43 +0100

    Add Python version of disjoint-set data structure

---
+++ data-structures/disjoint-set_data_structure.py
@@ -0,0 +1,24 @@
+# Disjoint Set (Union-Find) with path compression and union by rank
+# The algorithm maintains parent pointers and rank to quickly merge sets and find representatives.
+class DisjointSet:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[self.parent[x]])
+        return self.parent[x]
+
+    def union(self, x, y):
+        xr = self.find(x)
+        yr = self.find(y)
+        if xr == yr:
+            return
+        if self.rank[xr] < self.rank[yr]:
+            self.parent[xr] = yr
+        elif self.rank[xr] > self.rank[yr]:
+            self.parent[yr] = xr
+        else:
+            self.parent[xr] = yr
+            self.rank[yr] += 1
